9f579e43309c8ab20de07bad8ba16976b2948523
thinkreed <thinkreed2017@outlook.com>
2017-07-28 08:15:52 +0800

    camera2 step 2

--- app/src/main/java/reed/flyingreed/experimental/camera/Camera2Fragment.kt
+++ app/src/main/java/reed/flyingreed/experimental/camera/Camera2Fragment.kt
@@ -1,27 +1,85 @@
 package reed.flyingreed.experimental.camera
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_camera2.*
+import kotlinx.coroutines.experimental.newCoroutineContext
+import reed.flyingreed.R
 
 /**
  * Created by thinkreed on 2017/7/27.
  */
 class Camera2Fragment : Fragment() {
 
+    private var mState = State.IDLE
+    private var mWorkerThread: HandlerThread? = null
+    private var mHandler: Handler? = null
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater?.inflate(R.layout.fragment_camera2, container, false)
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        record.setOnClickListener {
+            when (mState) {
+                State.IDLE -> {
+                    startRecordVideo()
+                }
+                State.RECODING -> {
+                    stopRecordVideo()
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startWorker()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopWorker()
+    }
+
+    private fun startRecordVideo() {
+
+    }
+
+    private fun stopRecordVideo() {
+
+    }
+
+    private fun startWorker() {
+        val worker = HandlerThread("camera_worker")
+        mWorkerThread = worker
+        worker.start()
+        mHandler = Handler(worker.looper)
+    }
+
+    private fun stopWorker() {
+        val worker = mWorkerThread
+        if (worker != null) {
+            worker.quitSafely()
+            worker.join()
+            mWorkerThread = null
+            mHandler = null
+        }
     }
 
     companion object {
         fun getInstance(): Camera2Fragment {
             return Camera2Fragment()
         }
+    }
+
+    enum class State {
+        RECODING, IDLE
     }
 }
