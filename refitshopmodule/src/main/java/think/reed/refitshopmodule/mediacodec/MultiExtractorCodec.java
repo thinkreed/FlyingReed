dc296e6ba6c1e4f7b7d0d87eab575a345c2f8090
thinkreed <thinkreed2017@outlook.com>
2017-08-21 23:48:42 +0800

    add multi extractor

---
+++ refitshopmodule/src/main/java/think/reed/refitshopmodule/mediacodec/MultiExtractorCodec.java
@@ -0,0 +1,18 @@
+package think.reed.refitshopmodule.mediacodec;
+
+import android.media.MediaExtractor;
+
+import java.util.HashMap;
+
+/**
+ * multi extractor codec, smooth switch tiny media files
+ * Created by thinkreed on 2017/8/21.
+ */
+
+public class MultiExtractorCodec {
+    private HashMap<String, MediaExtractor> extractors = new HashMap<>();
+
+    private void config(String path) {
+
+    }
+}
