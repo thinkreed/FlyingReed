6d8b88f0a0365ecc3ac20b342e039d01a132e6af
thinkreed <thinkreed2017@outlook.com>
2017-08-22 22:56:56 +0800

    multi extractor 3

--- refitshopmodule/src/main/java/think/reed/refitshopmodule/mediacodec/MultiExtractorCodec.java
+++ refitshopmodule/src/main/java/think/reed/refitshopmodule/mediacodec/MultiExtractorCodec.java
@@ -1,8 +1,12 @@
 package think.reed.refitshopmodule.mediacodec;
 
 import android.media.MediaExtractor;
+import android.media.MediaFormat;
+import android.support.annotation.Nullable;
+import android.util.SparseArray;
 
-import java.util.HashMap;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * multi extractor codec, smooth switch tiny media files
@@ -10,9 +14,47 @@ import java.util.HashMap;
  */
 
 public class MultiExtractorCodec {
-    private HashMap<String, MediaExtractor> extractors = new HashMap<>();
 
-    private void config(String path) {
+    private List<String> pathList;
+
+    private int curIndex = -1;
+
+    private SparseArray<MediaExtractor> extractors = new SparseArray<>();
+
+    public MultiExtractorCodec() {
+        initExtractors();
+    }
+
+    private void initExtractors() {
+        for (int i = 0; i < 2; i++) {
+            extractors.put(i, new MediaExtractor());
+        }
+    }
+
+    @Nullable
+    private MediaExtractor switchExtractor() {
+        try {
+            curIndex = (curIndex + 1) % extractors.size();
+            MediaExtractor extractor = extractors.get(curIndex);
+            extractor.setDataSource(pathList.remove(0));
+            return selectAudioTrack(extractor) ? extractor : null;
+        } catch (NullPointerException e) {
+            return null;
+        } catch (IndexOutOfBoundsException e) {
+            return null;
+        } catch (IOException e) {
+            return null;
+        }
 
     }
+
+    private boolean selectAudioTrack(MediaExtractor extractor) {
+        for (int i = 0; i < extractor.getTrackCount(); i++) {
+            MediaFormat format = extractor.getTrackFormat(i);
+            String mime = format.getString(MediaFormat.KEY_MIME);
+            if (mime.startsWith("audio/")) return true;
+        }
+        return false;
+    }
+
 }
