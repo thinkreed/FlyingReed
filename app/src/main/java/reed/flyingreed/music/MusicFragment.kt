7cde8e5b3044b6a8c5cd2657acf821376273a632
thinkreed <thinkreed2017@outlook.com>
2017-09-19 22:40:38 +0800

    refacoter music fragment

--- app/src/main/java/reed/flyingreed/music/MusicFragment.kt
+++ app/src/main/java/reed/flyingreed/music/MusicFragment.kt
@@ -11,12 +11,21 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_music.*
+import kotlinx.android.synthetic.main.fragment_music.friday
+import kotlinx.android.synthetic.main.fragment_music.monday
+import kotlinx.android.synthetic.main.fragment_music.thursday
+import kotlinx.android.synthetic.main.fragment_music.tuesday
+import kotlinx.android.synthetic.main.fragment_music.wednesday
 import reed.flyingreed.IPlayerService
 import reed.flyingreed.R
-import reed.flyingreed.common.services.PlayerService
 import reed.flyingreed.common.model.Const
 import reed.flyingreed.common.model.Week
+import reed.flyingreed.common.model.Week.FRIDAY
+import reed.flyingreed.common.model.Week.MONDAY
+import reed.flyingreed.common.model.Week.THURSDAY
+import reed.flyingreed.common.model.Week.TUESDAY
+import reed.flyingreed.common.model.Week.WEDNESDAY
+import reed.flyingreed.common.services.PlayerService
 import reed.flyingreed.music.activity.MusicPlayerActivity
 
 /**
@@ -25,132 +34,143 @@ import reed.flyingreed.music.activity.MusicPlayerActivity
 
 class MusicFragment : Fragment(), View.OnClickListener {
 
-    private var mPlayerService: IPlayerService? = null
-    private var mBlinkingView:View? = null
+  private var mPlayerService: IPlayerService? = null
+  private var mBlinkingView: View? = null
 
-    private val mHandler by lazy {
-        Handler()
+  private val mHandler by lazy {
+    Handler()
+  }
+
+  private val mServiceConnection by lazy {
+    object : ServiceConnection {
+      override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+        mPlayerService = IPlayerService.Stub.asInterface(service)
+      }
+
+      override fun onServiceDisconnected(name: ComponentName?) {
+      }
+    }
+  }
+
+
+  override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+      savedInstanceState: Bundle?): View? {
+    return inflater?.inflate(R.layout.fragment_music, container, false)
+  }
+
+  override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    view?.let {
+      setOnClickListeners()
+      bindToMusicService()
+    }
+  }
+
+  private fun setOnClickListeners() {
+    monday.setOnClickListener(this)
+    tuesday.setOnClickListener(this)
+    wednesday.setOnClickListener(this)
+    thursday.setOnClickListener(this)
+    friday.setOnClickListener(this)
+  }
+
+  private fun bindToMusicService(): Boolean {
+    val intent = Intent(activity, PlayerService::class.java)
+    return activity.bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE)
+  }
+
+  override fun onClick(v: View?) {
+    v?.let {
+      openPlayer(selectFavor(v))
+    }
+  }
+
+  private fun selectFavor(v: View): Int {
+    when (v.id) {
+      R.id.monday -> {
+        return MONDAY.ordinal
+      }
+      R.id.tuesday -> {
+        return TUESDAY.ordinal
+
+      }
+      R.id.wednesday -> {
+        return WEDNESDAY.ordinal
+      }
+      R.id.thursday -> {
+        return THURSDAY.ordinal
+      }
+      R.id.friday -> {
+        return FRIDAY.ordinal
+      }
+      else -> return MONDAY.ordinal
+    }
+  }
+
+  private fun openPlayer(week: Int) {
+    val intent = Intent(this.context, MusicPlayerActivity::class.java)
+    val bundle = Bundle()
+    bundle.putInt(Const.KEY_WEEK, week)
+    intent.putExtras(bundle)
+    startActivity(intent)
+  }
+
+  override fun onResume() {
+    super.onResume()
+    if (mPlayerService!!.isPlaying) {
+      mHandler.post(ShiningTask(getBlinkingView()))
     }
 
-    private val mServiceConnection by lazy {
-        object : ServiceConnection {
-            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-                mPlayerService = IPlayerService.Stub.asInterface(service)
-            }
+  }
 
-            override fun onServiceDisconnected(name: ComponentName?) {
-            }
-        }
+  private fun getBlinkingView(): View {
+    when (Week.values()[mPlayerService!!.favor]) {
+      MONDAY -> {
+        return monday
+      }
+      TUESDAY -> {
+        return tuesday
+      }
+      WEDNESDAY -> {
+        return wednesday
+      }
+      THURSDAY -> {
+        return thursday
+      }
+      FRIDAY -> {
+        return friday
+      }
     }
+  }
 
+  override fun onStop() {
+    //reset the blinking view
+    mBlinkingView?.animate()?.alpha(1.0f)?.duration = 0
+    mHandler.removeCallbacksAndMessages(null)
+    super.onStop()
+  }
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.fragment_music, container, false)
+  override fun onDestroyView() {
+    activity.unbindService(mServiceConnection)
+    super.onDestroyView()
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+  }
+
+  inner class ShiningTask(var view: View) : Runnable {
+    override fun run() {
+      val alpha = if (view.alpha == 0f) 1f else 0f
+      view.animate().alpha(alpha).duration = 1000
+      //set the delay time a little longer than duration to ensure animation ended
+      mHandler.postDelayed(this, 1100)
     }
+  }
 
-    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        view?.let {
-            monday.setOnClickListener(this)
-            tuesday.setOnClickListener(this)
-            wednesday.setOnClickListener(this)
-            thursday.setOnClickListener(this)
-            friday.setOnClickListener(this)
-            val intent = Intent(activity, PlayerService::class.java)
-            activity.bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE)
-        }
+  companion object {
+    fun getInstance(): MusicFragment {
+      return MusicFragment()
     }
-
-    override fun onClick(v: View?) {
-        v?.let {
-            var week = 0
-            when (v.id) {
-                R.id.monday -> {
-                    week = Week.MONDAY.ordinal
-                }
-                R.id.tuesday -> {
-                    week = Week.TUESDAY.ordinal
-
-                }
-                R.id.wednesday -> {
-                    week = Week.WEDNESDAY.ordinal
-
-                }
-                R.id.thursday -> {
-                    week = Week.THURSDAY.ordinal
-
-                }
-                R.id.friday -> {
-                    week = Week.FRIDAY.ordinal
-
-                }
-                else -> IllegalArgumentException("not a weekday")
-            }
-            val intent = Intent(this.context, MusicPlayerActivity::class.java)
-            val bundle = Bundle()
-            bundle.putInt(Const.KEY_WEEK, week)
-            intent.putExtras(bundle)
-            startActivity(intent)
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        val service = mPlayerService
-        if (service != null) {
-            if (service.isPlaying) {
-                val view = when (Week.values()[service.favor]) {
-                    Week.MONDAY -> {
-                        monday
-                    }
-                    Week.TUESDAY -> {
-                        tuesday
-                    }
-                    Week.WEDNESDAY -> {
-                        wednesday
-                    }
-                    Week.THURSDAY -> {
-                        thursday
-                    }
-                    Week.FRIDAY -> {
-                        friday
-                    }
-                }
-                mBlinkingView = view
-                mHandler.post(ShiningTask(view))
-            }
-        }
-
-    }
-
-    override fun onStop() {
-        //reset the blinking view
-        mBlinkingView?.animate()?.alpha(1.0f)?.duration = 0
-        mHandler.removeCallbacksAndMessages(null)
-        super.onStop()
-    }
-
-    override fun onDestroyView() {
-        activity.unbindService(mServiceConnection)
-        super.onDestroyView()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
-
-    inner class ShiningTask(var view: View) : Runnable {
-        override fun run() {
-            val alpha = if (view.alpha == 0f) 1f else 0f
-            view.animate().alpha(alpha).duration = 1000
-            //set the delay time a little longer than duration to ensure animation ended
-            mHandler.postDelayed(this, 1100)
-        }
-    }
-
-    companion object {
-        fun getInstance(): MusicFragment {
-            return MusicFragment()
-        }
-    }
+  }
 }
